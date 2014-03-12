c46b0380f465bd02b4274a16012b0ba09e477518
Ryan Printup <ryan.printup@gmail.com>
2014-03-11 23:09:39 -0400

    Combined with Client.java

--- src/com/ryanprintup/starsector/Player.java
+++ src/com/ryanprintup/starsector/Player.java
@@ -1,21 +1,70 @@
 package com.ryanprintup.starsector;
 
+import java.io.IOException;
+import java.net.Socket;
+
+import com.ryanprintup.starsector.net.Connection;
+import com.ryanprintup.starsector.util.Config;
+
 public class Player
 {
-	private final String name;
-	private final String UUID;
+	private Socket socket;
+	private Connection client;
+	private Connection server;
 	
-	public Player(final String name, final String UUID)
+	private String name;
+	private String ip;
+	
+	public Player(final Socket socket)
 	{
-		this.name = name;
-		this.UUID = UUID;
+		this.socket = socket;
+		ip = socket.getInetAddress().getHostAddress();
+		
+		try {
+			socket.setKeepAlive(true);
+			Socket serverSocket = new Socket("127.0.0.1", Config.getServerInstance().getConfig().getStarboundPort());
+			
+			client = new Connection(socket.getInputStream(), serverSocket.getOutputStream());
+			server = new Connection(serverSocket.getInputStream(), socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public String getName() {
+	public void sendMessage(String message)
+	{
+		
+	}
+	
+	private void sendPacket(final byte packet[])
+	{
+		try {
+			socket.getOutputStream().write(packet);
+			socket.getOutputStream().flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void disconnect()
+	{
+		client.endConnection();
+		server.endConnection();
+
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getName()
+	{
 		return name;
 	}
-
-	public String getUUID() {
-		return UUID;
+	
+	public String getIp()
+	{
+		return ip;
 	}
 }
