ef8b99f5e15de8c0f8f42dd493e298c7ab8e9714
Ryan Printup <ryan.printup@gmail.com>
2014-03-11 23:06:46 -0400

    Rewrote for more functionallity

--- src/com/ryanprintup/starsector/command/Command.java
+++ src/com/ryanprintup/starsector/command/Command.java
@@ -1,15 +1,53 @@
 package com.ryanprintup.starsector.command;
 
-import com.ryanprintup.starsector.Client;
+import com.ryanprintup.starsector.Player;
+import com.ryanprintup.starsector.Server;
+import com.ryanprintup.starsector.util.Config;
 import com.ryanprintup.starsector.util.Console;
 
-public interface Command
+public abstract class Command
 {
-	public static final Console console = Console.getInstance();
+	protected static Console console = Console.getInstance();
+	protected static Server server = Config.getServerInstance();
+	protected static CommandList commandList = new CommandList();
 	
-	public abstract void use(Client c, String command);
-	public abstract void help(Client c);
+	public void use(Player player, String command) {
+		if (player == null) {
+			if (!allowConsole()) {
+				console.write("This command can't be used in the console!");
+				return;
+			}
+		} else {
+			if (!allowPlayer()) {
+				player.sendMessage("This command can't be used by a player!");
+				return;
+			}
+		}
+		
+		
+		// Remove command name
+		if (command.contains(" ")) {
+			String args = command.substring(command.indexOf(" ") + 1, command.length());
+			
+			onUse(player, args);
+		} else {
+			onUse(player, null);
+		}
+	}
+	
+	public void help(Player player) {
+		if (player == null) {
+			console.write(getHelp());
+		} else {
+			player.sendMessage("/" + getHelp());
+		}
+	}
+	
+	public abstract void onUse(Player player, String args);
 	
 	public abstract String getName();
 	public abstract String getShortcut();
+	public abstract boolean allowConsole();
+	public abstract boolean allowPlayer();
+	public abstract String getHelp();
 }
--- src/com/ryanprintup/starsector/command/CommandList.java
+++ src/com/ryanprintup/starsector/command/CommandList.java
@@ -3,18 +3,58 @@ package com.ryanprintup.starsector.command;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommandList {
+public class CommandList
+{
+	private static List<Command> commands = new ArrayList<Command>();
 	
-	private static List<Command> commandList = new ArrayList<Command>();
-	
-	public  void add(Command cmd)
+	public void add(Command cmd)
 	{
-		commandList.add(cmd);
+		commands.add(cmd);
 	}
 	
-	// Init all default commands here
-	private void init()
+	public Command getCommand(String command)
 	{
-		commandList.add(new CmdExample());
+		for (Command cmd : commands) {
+			if (cmd.getName().equals(command) || cmd.getShortcut().equals(command)) {
+				return cmd;
+			}
+		}
+		
+		return null;
 	}
+	
+	public boolean isCommand(String command)
+	{
+		return getCommand(command) == null ? false : true;
+	}
+	
+	public Command getCommand(Command command)
+	{
+		return getCommand(command.getName());
+	}
+	
+	public Command getCommand(int index)
+	{
+		if (index > commands.size()) {
+			return null;
+		}
+		
+		return commands.get(index);
+	}
+	
+	public int getSize()
+	{
+		return commands.size();
+	}
+	
+	// Initiate all default commands here
+		public static void initDefaults()
+		{
+			commands.add(new CmdHelp());
+			commands.add(new CmdIp());
+			commands.add(new CmdKick());
+			commands.add(new CmdSay());
+			commands.add(new CmdPrivateMessage());
+			commands.add(new CmdPlayers());
+		}
 }
