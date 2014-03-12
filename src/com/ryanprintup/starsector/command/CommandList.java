9c76fa1e3808cfdaefb661f13115884879a2bae1
Ryan Printup <ryan.printup@gmail.com>
2014-03-12 14:50:14 -0400

    Added more commands

--- src/com/ryanprintup/starsector/command/CommandList.java
+++ src/com/ryanprintup/starsector/command/CommandList.java
@@ -7,13 +7,15 @@ public class CommandList
 {
 	private static List<Command> commands = new ArrayList<Command>();
 	
-	public void add(Command cmd)
+	public void add(Command command)
 	{
-		commands.add(cmd);
+		commands.add(command);
 	}
 	
 	public Command getCommand(String command)
 	{
+		command = command.toLowerCase();
+		
 		for (Command cmd : commands) {
 			if (cmd.getName().equals(command) || cmd.getShortcut().equals(command)) {
 				return cmd;
@@ -21,11 +23,6 @@ public class CommandList
 		}
 		
 		return null;
-	}
-	
-	public boolean isCommand(String command)
-	{
-		return getCommand(command) == null ? false : true;
 	}
 	
 	public Command getCommand(Command command)
@@ -42,19 +39,26 @@ public class CommandList
 		return commands.get(index);
 	}
 	
+	public boolean isCommand(String command)
+	{
+		return getCommand(command) == null ? false : true;
+	}
+	
 	public int getSize()
 	{
 		return commands.size();
 	}
 	
 	// Initiate all default commands here
-		public static void initDefaults()
+	public static void initDefaults()
 		{
 			commands.add(new CmdHelp());
 			commands.add(new CmdIp());
 			commands.add(new CmdKick());
 			commands.add(new CmdSay());
+			commands.add(new CmdStart());
 			commands.add(new CmdPrivateMessage());
 			commands.add(new CmdPlayers());
+			commands.add(new CmdStop());
 		}
 }
