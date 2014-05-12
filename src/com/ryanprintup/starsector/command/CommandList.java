777812cdc7eb30ce402b45f2a28a3543816b2f77
Ryan Printup <ryan.printup@gmail.com>
2014-05-12 15:39:12 -0400

    Defaults now handled in server class

--- src/com/ryanprintup/starsector/command/CommandList.java
+++ src/com/ryanprintup/starsector/command/CommandList.java
@@ -3,11 +3,21 @@ package com.ryanprintup.starsector.command;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ryanprintup.starsector.command.defaults.CmdHelp;
+import com.ryanprintup.starsector.command.defaults.CmdIp;
+import com.ryanprintup.starsector.command.defaults.CmdKick;
+import com.ryanprintup.starsector.command.defaults.CmdPlayers;
+import com.ryanprintup.starsector.command.defaults.CmdPrivateMessage;
+import com.ryanprintup.starsector.command.defaults.CmdRestart;
+import com.ryanprintup.starsector.command.defaults.CmdSay;
+import com.ryanprintup.starsector.command.defaults.CmdStart;
+import com.ryanprintup.starsector.command.defaults.CmdStop;
+
 public class CommandList
 {
 	private static List<Command> commands = new ArrayList<Command>();
 	
-	public void add(Command command)
+	public void register(Command command)
 	{
 		commands.add(command);
 	}
@@ -48,18 +58,4 @@ public class CommandList
 	{
 		return commands.size();
 	}
-	
-	// Initiate all default commands here
-	public static void initDefaults()
-		{
-			commands.add(new CmdHelp());
-			commands.add(new CmdIp());
-			commands.add(new CmdKick());
-			commands.add(new CmdSay());
-			commands.add(new CmdStart());
-			commands.add(new CmdPrivateMessage());
-			commands.add(new CmdRestart());
-			commands.add(new CmdPlayers());
-			commands.add(new CmdStop());
-		}
 }
