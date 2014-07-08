20041451742d3dcfb7db20ecbccc9f9c00cb5454
Ryan Printup <ryan.printup@gmail.com>
2014-07-07 20:27:55 -0400

    Testing decoding on chat packet

--- src/com/ryanprintup/starsector/net/PacketProcessor.java
+++ src/com/ryanprintup/starsector/net/PacketProcessor.java
@@ -1,30 +1,46 @@
 package com.ryanprintup.starsector.net;
 
-import com.ryanprintup.starsector.net.packets.Packets;
 import com.ryanprintup.starsector.util.ZlibCompressor;
+
 
 
 public class PacketProcessor
 {	
-	public void process(final byte[] packet)
+	public void process(final byte[] buffer)
 	{
-		switch (packet[0]) {
-			case Packets.protocolVersion:
-				System.out.println("Server protocol");
-				break;
-			case Packets.connectionResponse:
-				System.out.println("Client join successful");
-				break;
-			case Packets.chatRecieved:
-				System.out.println("Chat message");
-				break;
-			case Packets.clientConnect:
-				System.out.println("Client connect");
-				break;
-			case Packets.clientDisconnect:
-				System.out.println("Client disconnect");
-				break;
-		}
+			PacketReader pr = new PacketReader(buffer);
+			int packetId = pr.readByte();
+			
+			if (packetId != 0x04) {
+				return;
+			}
+			System.out.println(packetId);
+			
+			long payload = pr.readSVLQ();
+			
+			boolean compressed = payload < 0;
+			
+			if (compressed) {
+				payload = -payload;
+				System.out.println("negative");
+			}
+			
+			System.out.println(packetId + " | " + payload);
+			
+			PacketReader reader;
+			
+			if (compressed) {
+				reader = new PacketReader(ZlibCompressor.decompress(pr.readByteArray((int) payload)));
+			} else {
+				reader = new PacketReader(pr.readByteArray((int) payload));
+			}
+			
+			int channel = reader.readUInt8();
+			String world = reader.readString();
+			long clientId = reader.readUInt32();
+			String name = reader.readString();
+			String message = reader.readString();
+			
+			System.out.println(channel + " | " + world + " | " + clientId + " | " + name + " | " + message);
 	}
-	
 }
