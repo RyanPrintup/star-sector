98df1c09441f28ad75323498cd247568495e9510
Ryan Printup <ryan.printup@gmail.com>
2014-07-10 18:54:06 -0400

    Added sendMessage(String[] messages)

--- src/com/ryanprintup/starsector/command/CommandSender.java
+++ src/com/ryanprintup/starsector/command/CommandSender.java
@@ -3,4 +3,6 @@ package com.ryanprintup.starsector.command;
 public interface CommandSender
 {
 	public abstract void sendMessage(String message);
+	
+	public abstract void sendMessage(String[] messages);
 }
