cbc0cad60916c8dbc8bab3ceb77d7ce537d563a8
Ryan Printup <ryan.printup@gmail.com>
2015-04-01 16:38:17 -0400

    Add setters

--- src/main/java/com/ryanprintup/starsector/configuration/StarSectorConfig.java
+++ src/main/java/com/ryanprintup/starsector/configuration/StarSectorConfig.java
@@ -2,6 +2,7 @@ package com.ryanprintup.starsector.configuration;
 
 public class StarSectorConfig extends Config
 {
+    public static final String SERVER_NAME    = "serverName";
     public static final String PORT           = "serverPort";
     public static final String STARBOUND_PORT = "starboundPort";
     public static final String MAX_PLAYERS    = "maxPlayers";
@@ -13,9 +14,35 @@ public class StarSectorConfig extends Config
 
     protected void setDefaults()
     {
+        set(SERVER_NAME,    "Star Sector");
         set(PORT,           21020);
         set(STARBOUND_PORT, 21025);
         set(MAX_PLAYERS,    25);
+    }
+
+    public void setServerName(String value)
+    {
+        set(SERVER_NAME, value);
+    }
+
+    public void setPort(int value)
+    {
+        set(PORT, value);
+    }
+
+    public void setStarboundPort(int value)
+    {
+        set(STARBOUND_PORT, value);
+    }
+
+    public void setMaxPlayers(int value)
+    {
+        set(MAX_PLAYERS, value);
+    }
+
+    public String getServerName()
+    {
+        return getString(SERVER_NAME);
     }
 
     public int getPort()
--- src/main/java/com/ryanprintup/starsector/configuration/StarboundConfig.java
+++ src/main/java/com/ryanprintup/starsector/configuration/StarboundConfig.java
@@ -5,7 +5,6 @@ public class StarboundConfig extends Config
     public static final String PORT             = "gameServerPort";
     public static final String SERVER_NAME      = "serverName";
     public static final String MAX_PLAYERS      = "maxPlayers";
-    public static final String SERVER_PASSWORDS = "serverPasswords";
 
     public StarboundConfig(String path)
     {
@@ -17,7 +16,21 @@ public class StarboundConfig extends Config
         set(PORT,             21025);
         set(SERVER_NAME,      "Star Sector Server");
         set(MAX_PLAYERS,      25);
-        set(SERVER_PASSWORDS, null);
+    }
+
+    public void setPort(int value)
+    {
+        set(PORT, value);
+    }
+
+    public void setServerName(String value)
+    {
+        set(SERVER_NAME, value);
+    }
+
+    public void setMaxPlayers(int value)
+    {
+        set(MAX_PLAYERS, value);
     }
 
     public int getPort()
@@ -33,10 +46,5 @@ public class StarboundConfig extends Config
     public int getMaxPlayers()
     {
         return getInt(MAX_PLAYERS);
-    }
-
-    public String getServerPasswords()
-    {
-        return getString(SERVER_PASSWORDS);
     }
 }
