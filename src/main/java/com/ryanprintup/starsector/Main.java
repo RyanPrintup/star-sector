5e0a34883a76a837c2a12632ab851b61698e0f68
Ryan Printup <ryan.printup@gmail.com>
2015-04-01 16:38:29 -0400

    Remove test code

--- src/main/java/com/ryanprintup/starsector/Main.java
+++ src/main/java/com/ryanprintup/starsector/Main.java
@@ -1,21 +1,9 @@
 package com.ryanprintup.starsector;
 
-import com.ryanprintup.starsector.configuration.StarSectorConfig;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
-
 public class Main
 {
 	public static void main(String[] args)
-	{
-        StarSectorConfig config = new StarSectorConfig("starsector.config");
-        try {
-            config.load();
-            System.out.println(config.getMaxPlayers() + " " + config.getPort());
-        } catch (IOException | ParseException e) {
-
-        }
-        // new Server().start();
+    {
+        new Server();
 	}
 }
