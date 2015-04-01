598ddd36f703ba67c9df25f0a3c07f6286b510a7
Ryan Printup <ryan.printup@gmail.com>
2015-04-01 16:40:28 -0400

    Add constructor

--- src/main/java/com/ryanprintup/starsector/datatypes/NetTile.java
+++ src/main/java/com/ryanprintup/starsector/datatypes/NetTile.java
@@ -18,7 +18,32 @@ public class NetTile
 	private byte unknown13;
 	private byte liquidLevel;
 	private long gravity;
-	
+
+    public NetTile(short unknown0, byte unknown1, byte unknown2,
+                   short unknown3, byte unknown4, short unknown5,
+                   byte unknown6, byte unknown7, short unknown8,
+                   byte unknown9, byte unknown10, byte unknown11,
+                   byte unknown12, byte unknown13, byte liquidLevel,
+                   long gravity)
+    {
+        this.unknown0    = unknown0;
+        this.unknown1    = unknown1;
+        this.unknown2    = unknown2;
+        this.unknown3    = unknown3;
+        this.unknown4    = unknown4;
+        this.unknown5    = unknown5;
+        this.unknown6    = unknown6;
+        this.unknown7    = unknown7;
+        this.unknown8    = unknown8;
+        this.unknown9    = unknown9;
+        this.unknown10   = unknown10;
+        this.unknown11   = unknown11;
+        this.unknown12   = unknown12;
+        this.unknown13   = unknown13;
+        this.liquidLevel = liquidLevel;
+        this.gravity     = gravity;
+    }
+
 	public short getUnknown0()
 	{
 		return unknown0;
